2046e7e299ac19c98d0c97989664891d34ffb5fd
chengji253 <liuhan253@163.com>
2018-08-20 11:25:39 +0800

    1

--- c++/c++_basic-mission/1.4_CPolynomial_list/CPolynomial_List_test.cpp
+++ c++/c++_basic-mission/1.4_CPolynomial_list/CPolynomial_List_test.cpp
@@ -2,8 +2,7 @@
 
 int main()
 {
-    // FIXME: please put P3.txt, P4.txt in the folder,
-    //  thus it can be easy to test the program
+    
  	CPolynomial p1("P3.txt");
  	CPolynomial p2("P4.txt");
  	CPolynomial p3;
